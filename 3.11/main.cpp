d734ca764f526894e2d5de7ac6ee0cc48e58a556
guixv <44542313+guixv@users.noreply.github.com>
2022-02-04 01:57:23 +0800

    3.11

---
+++ 3.11/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "input:" << endl;
+    cin >> a >> b;
+    cout << "a+b="<<a+b<<endl;
+    cout << "a-b="<<a-b<<endl;
+    cout << "a*b="<<a*b<<endl;
+    cout << "a/b="<<a/b<<endl;
+
+
+    return 0;
+}
