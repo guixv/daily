49ec3847ca4147fc3a4c44fd9c3cd510e3f8a455
guixv <44542313+guixv@users.noreply.github.com>
2022-02-10 00:05:51 +0800

    3.19.3

---
+++ 3.19/3.19.3/cmake-build-debug/CMakeFiles/3_19_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_19_3.dir/main.cpp.obj"
+  "3_19_3.pdb"
+  "3_19_3.exe"
+  "3_19_3.exe.manifest"
+  "lib3_19_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_19_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3.19/3.19.3/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+namespace X{
+    int var;
+    void print(){
+        cout<<"x:"<<var<<endl;
+    }
+};
+
+namespace Y{
+    int var;
+    void print(){
+        cout<<"y:"<<var<<endl;
+    }
+};
+
+namespace Z{
+    int var;
+    void print(){
+        cout<<"z:"<<var<<endl;
+    }
+};
+
+int main()
+{
+
+    X::var = 7;
+    X::print();                          //print X’s var
+    using namespace Y ;
+    var = 9;
+    print();	                           //print Y’s var
+    {
+        using Z::var;
+        using Z::print;
+        var = 11;
+        print();                         //print Z’s var
+    }
+    print();                            //print Y’s var
+    X::print();                          //print X’s var
+    return 0;
+} ;
